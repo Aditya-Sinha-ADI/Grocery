520c9c7202f3f05336b75356594e6b814ef36cce
AshwinTomar <ashwintomar04@gmail.com>
2022-02-27 13:06:51 +0530

    Fixed some routes and issues

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,5 @@
-exports.getAllProducts = (req,res)=>{
-    res.status(200).json({message : "Product route is working properly"});
+const getAllProducts = (req, res) => {
+    res.status(200).json({ message: "Product route is working properly" });
 }
+
+module.exports = getAllProducts;
